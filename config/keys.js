3a949d93bf845865c71a8b61c53536aa6d9e14aa
Branko Vukovic <bvukovic082@gmail.com>
2019-03-12 17:54:43 +0100

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://bane:bane12081982@ds151513.mlab.com:51513/todobangula'
+};
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const db = require('./config/keys').mongoURI;
+//Conect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected!'))
+  .catch(err => console.log(err, 'Problem connecting to mongoDB!'))
+
+
+
+//Apply middleware BodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Import routes
+const users = require('./routes/api/users');
+
+app.use('/api/users', users);
+
+app.listen(port, () => console.log(`Server is running on port: ${port}`));

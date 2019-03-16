b21032efa91aaaaa69767058233520b8445b9397
Branko Vukovic <bvukovic082@gmail.com>
2019-03-16 11:15:15 +0100

    seting up static assets for production

--- server.js
+++ server.js
@@ -23,6 +23,16 @@ const users = require("./routes/api/users");
 
 app.use("/api/users", users);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const server = app.listen(port, () =>
   console.log(`Server is running on port: ${port}`)
 );
